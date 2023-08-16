92ad1b9e5e91adf5bbea4bd5c7787adc0d79f73f
Sergio Festa <71560110+sgfesta@users.noreply.github.com>
2023-08-16 12:54:57 -0300

    boton cerrar formulario

--- src/detodosa/BusquedaPorNombreView.java
+++ src/detodosa/BusquedaPorNombreView.java
@@ -43,6 +43,7 @@ public class BusquedaPorNombreView extends javax.swing.JInternalFrame {
         jtNombre = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         jtProductos = new javax.swing.JTable();
+        jbCerrar = new javax.swing.JButton();
 
         jLabel1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jLabel1.setText("Listado por Nombre");
@@ -61,6 +62,13 @@ public class BusquedaPorNombreView extends javax.swing.JInternalFrame {
             }
         ));
         jScrollPane1.setViewportView(jtProductos);
+
+        jbCerrar.setText("Cerrar");
+        jbCerrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbCerrarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -81,6 +89,9 @@ public class BusquedaPorNombreView extends javax.swing.JInternalFrame {
                 .addGap(124, 124, 124)
                 .addComponent(jLabel1)
                 .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jbCerrar))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -93,17 +104,24 @@ public class BusquedaPorNombreView extends javax.swing.JInternalFrame {
                     .addComponent(jtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(54, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addComponent(jbCerrar))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jbCerrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbCerrarActionPerformed
+        //Para cerrar el formulario actual
+        this.dispose();
+    }//GEN-LAST:event_jbCerrarActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton jbCerrar;
     private javax.swing.JTextField jtNombre;
     private javax.swing.JTable jtProductos;
     // End of variables declaration//GEN-END:variables
